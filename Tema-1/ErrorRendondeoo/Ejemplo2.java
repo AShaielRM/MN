cc3ebfcd2e30f14294028f138df88030c034e2b8
AShaielRM <147759362+AShaielRM@users.noreply.github.com>
2025-05-29 09:44:18 -0600

    Create Ejemplo2.java

---
+++ Tema-1/ErrorRendondeoo/Ejemplo2.java
@@ -0,0 +1,7 @@
+public class ErrorRedondeo2 {
+    public static void main(String[] args) {
+        double resultado = 0.1 * 3;
+
+        RedondeoUtils.mostrar("0.1 * 3", resultado);  // Esperado: 0.3 exacto
+    }
+}
