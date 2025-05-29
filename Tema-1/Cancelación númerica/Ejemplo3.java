8345a138a6187ffd3ae1d290c5ab34f5c5ad7085
AShaielRM <147759362+AShaielRM@users.noreply.github.com>
2025-05-29 09:31:43 -0600

    Create Ejemplo3.java

---
+++ Tema-1/Cancelación númerica/Ejemplo3.java
@@ -0,0 +1,9 @@
+public class Cancelacion5 {
+    public static void main(String[] args) {
+        double x = 1234567.89;
+        double y = 1234567.88;
+        double diferencia = x - y;
+
+        Cancelacion1.mostrar("Diferencia pequeña entre grandes", diferencia);
+    }
+}
