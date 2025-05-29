772c6e2855592ee16dac7c11ce5e8e514173022d
AShaielRM <147759362+AShaielRM@users.noreply.github.com>
2025-05-29 09:45:45 -0600

    Create Ejemplo3.java

---
+++ Tema-1/ErrorRendondeoo/Ejemplo3.java
@@ -0,0 +1,7 @@
+public class ErrorRedondeo3 {
+    public static void main(String[] args) {
+        double division = 1.0 / 3.0;
+
+        RedondeoUtils.mostrar("1 / 3", division);  // Resultado periódico
+    }
+}
